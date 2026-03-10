12502c484b1039c85a1857fb549ec73944b82825
CostiganUY <91395025+CostiganUY@users.noreply.github.com>
2026-03-10 16:23:09 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -83,3 +83,41 @@ sec.classList.add("visible");
 window.addEventListener("scroll", revealSections);
 
 revealSections();
+
+/* LIGHTBOX GALERIA */
+
+const galleryImages = document.querySelectorAll(".gallery-img");
+
+const lightbox = document.getElementById("lightbox");
+
+const lightboxImg = document.getElementById("lightbox-img");
+
+const lightboxClose = document.getElementById("lightbox-close");
+
+galleryImages.forEach(img => {
+
+img.addEventListener("click", () => {
+
+lightbox.classList.add("active");
+
+lightboxImg.src = img.src;
+
+});
+
+});
+
+lightboxClose.addEventListener("click", () => {
+
+lightbox.classList.remove("active");
+
+});
+
+lightbox.addEventListener("click", e => {
+
+if(e.target !== lightboxImg){
+
+lightbox.classList.remove("active");
+
+}
+
+});
