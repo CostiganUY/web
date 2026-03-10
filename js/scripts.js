1106087329ec26aea41c21af109f24989d07f201
CostiganUY <91395025+CostiganUY@users.noreply.github.com>
2026-03-10 12:42:46 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -52,3 +52,19 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+window.addEventListener("scroll", function(){
+
+const nav = document.getElementById("mainNav");
+
+if(window.scrollY > 50){
+
+nav.classList.add("scrolled");
+
+}else{
+
+nav.classList.remove("scrolled");
+
+}
+
+});
