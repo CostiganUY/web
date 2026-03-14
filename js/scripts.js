5a380cb08d6223dbb9dc7dcbed80c6db191c8e33
CostiganUY <91395025+CostiganUY@users.noreply.github.com>
2026-03-14 12:06:19 -0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -150,19 +150,13 @@ const mobileMenu = document.querySelector(".mobile-menu");
 if(!toggle || !mobileMenu) return;
 
 toggle.addEventListener("click", () => {
-
 mobileMenu.classList.toggle("active");
-
 });
 
 document.querySelectorAll(".mobile-menu a").forEach(link => {
-
 link.addEventListener("click", () => {
-
 mobileMenu.classList.remove("active");
-
 });
-
 });
 
 });
